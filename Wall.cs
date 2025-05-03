578d1f52aa57f61b0120e6966ecae79a3cf8046f
hmekhjian <4435581+hmekhjian@users.noreply.github.com>
2025-05-03 15:10:46 +0100

    Add project files.

---
+++ Wall.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+
+namespace Architools
+{
+    public class Wall : Command
+    {
+        public Wall()
+        {
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static Wall Instance { get; private set; }
+
+        public override string EnglishName => "Wall";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: complete command.
+            return Result.Success;
+        }
+    }
+}
---
+++ toolCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace Architools
+{
+    public class toolCommand : Command
+    {
+        public toolCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static toolCommand Instance { get; private set; }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName => "toolCommand";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: start here modifying the behaviour of your command.
+            // ---
+
+
+
+            // ---
+            return Result.Success;
+        }
+    }
+}
