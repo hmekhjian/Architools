59d1d09d5d3be1e84c0d4c0e2f01031885623fc2
hmekhjian <4435581+hmekhjian@users.noreply.github.com>
2025-08-15 22:15:55 +0100

    Setup CreateRailing file

---
+++ Railing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Rhino;
+using Rhino.Commands;
+
+namespace Architools
+{
+    public class Railing : Command
+    {
+        public Railing()
+        {
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static Railing Instance { get; private set; }
+
+        public override string EnglishName => "CreateRailing";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: complete command.
+            return Result.Success;
+        }
+    }
+}
